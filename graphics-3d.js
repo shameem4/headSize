5d88c5785e1fa5899ae1d61b29dd3ed833b62b52
shameem4 <shameem.4@gmail.com>
2025-11-13 14:51:40 -0800

    feat: update Three.js imports to use import maps for better modularity

--- graphics-3d.js
+++ graphics-3d.js
@@ -12,8 +12,8 @@
  * - Configurable lighting and materials
  */
 
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { THREEJS_CONFIG, COLOR_CONFIG } from './config.js';
 
 // ============================================================================
